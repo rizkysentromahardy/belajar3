6457482f6a6d474764577705b74d157cded45a5f
unknown <rizkysentro3@gmail.com>
2021-07-19 06:54:17 +0700

    list view

--- lib/main.dart
+++ lib/main.dart
@@ -4,26 +4,54 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<Widget> widgets = [];
+  int counter = 1;
+  // _MyAppState() {
+  //   for (int i = 0; i < 15; i++)
+  //     widgets.add(Text(
+  //       "Data ke " + i.toString(),
+  //       style: TextStyle(fontSize: 35),
+  //     ));
+  // }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("belajar mengganti font"),
+          title: Text('belajar list view'),
         ),
-        body: Center(
-          child: Text(
-            'ini adalah text',
-            style: TextStyle(
-              fontFamily: 'SourceCode',
-              fontStyle: FontStyle.italic,
-              fontSize: 30,
-            ),
+        body: ListView(children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      widgets.add(Text('tambah data ' + counter.toString()));
+                      counter++;
+                    });
+                  },
+                  child: Text("tambah data")),
+              ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      widgets.removeLast();
+                      counter--;
+                    });
+                  },
+                  child: Text("kurang data")),
+            ],
           ),
-        ),
+          Column(children: widgets)
+        ]),
       ),
     );
   }
